01efc16a215584d46157abbac35ca156f2676869
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-12 21:18:33 -0400

    write logger function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code away!
+const express = require('express')
+const server = require('./server')
+const app = express();
+const cors = require('cors');
+
+// const blogRoutes = require('./blogRoutes');
+app.use(server)
+app.use(express.json());
+app.use(cors());
+
+// server.use('/api/users', usersRoutes);
+
+const PORT = 7000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}`);
+})
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
+const moment = require('moment')
+
 const server = express();
+
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`[${moment().format('lll')}] ${req.method} to ${req.url} from ${req.get('Origin')}`)
+  next();
+}
 
 module.exports = server;
