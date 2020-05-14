f3a9825367f99456bce90223bb34fc81acb81de9
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-14 15:45:47 -0400

    took out proxy

--- client/src/Axios/axiosInstance.js
+++ client/src/Axios/axiosInstance.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+const ENV = process.env.NODE_ENV === 'development' ?  'http://localhost:7000/api/users' : 'https://blog1234567.herokuapp.com/api/users'
 
 export default axios.create({
-  baseURL: 'http://localhost:7000/api/users'
+  baseURL: ENV
 });
 
 //'https://blog1234567.herokuapp.com/api/users' //http://localhost:7000/api/users'
+
+console.log(ENV)
