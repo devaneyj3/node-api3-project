e581468cede37fca482ae8687e5b6c1dee9edc13
Jordan Devaney <jordandevaney@Jordans-MacBook-Air.local>
2020-05-14 15:29:24 -0400

    add proxy

--- client/src/Axios/axiosInstance.js
+++ client/src/Axios/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 export default axios.create({
-  baseURL: 'https://blog1234567.herokuapp.com/api/users'
+  baseURL: 'http://localhost:7000/api/users'
 });
 
 //'https://blog1234567.herokuapp.com/api/users' //http://localhost:7000/api/users'
